a9f75787857c1c1467211704b5379006dd159dfd
bill <wjefferson.santos@gmail.com>
2022-05-23 19:08:36 -0300

    implementing a Logger class

--- src/debug/Log.java
+++ src/debug/Log.java
@@ -1,4 +1,20 @@
 package debug;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Log {
+    public static final boolean ISLOGABLE = true;
+    private static final String TAG = "PPOO";
+
+    public static void d(String subtag, String message){
+
+        System.out.println(getCurrentTimeFormatted() + " [" + TAG + "] [" + subtag + "] " + message);
+    }
+
+    private static String getCurrentTimeFormatted() {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss");
+        return dtf.format(LocalDateTime.now());
+    }
 }
